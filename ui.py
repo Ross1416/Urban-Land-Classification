6e2ef5a535d0052f70858b5a684a4784b97b1efe
Ross <77679842+Ross1416@users.noreply.github.com>
2025-03-07 17:25:58 +0000

    Added image and slider with correct panel layouts

--- ui.py
+++ ui.py
@@ -5,37 +5,46 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import sys
 
-
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
+        # Set main parameters
         self.setWindowTitle("Urban Land Classification")
         self.setGeometry(100, 100, 1200, 700)
 
+        # Set main layout
         main_layout = QHBoxLayout()
-        left_panel = self.create_selection_panel()
+        left_panel = self.create_left_layout()
+        centre_panel = self.create_centre_layout()
         right_panel = self.create_right_panel()
 
-        image_layout = QVBoxLayout()
-        self.image_label = QLabel("[Image Placeholder]")
-        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.slider = QSlider(Qt.Orientation.Horizontal)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(10)
-
-        image_layout.addWidget(self.image_label)
-        image_layout.addWidget(self.slider)
-
-        main_layout.addWidget(left_panel, 2)
-        main_layout.addLayout(image_layout, 5)
-        main_layout.addWidget(right_panel, 2)
+        main_layout.addLayout(left_panel, 2)
+        main_layout.addLayout(centre_panel, 5)
+        main_layout.addLayout(right_panel, 2)
 
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
-    def create_selection_panel(self):
+    def create_centre_layout(self):
+        image_container = self.create_image_panel()
+        slider_container = self.create_slider_panel()
+
+        layout = QVBoxLayout()
+        layout.addWidget(image_container)
+        layout.addWidget(slider_container)
+        return layout
+
+    def create_left_layout(self):
+        downloads_container = self.create_downloads_panel()
+        options_container = self.create_selections_panel()
+
+        layout = QVBoxLayout()
+        layout.addWidget(downloads_container)
+        layout.addWidget(options_container)
+        return layout
+
+    def create_selections_panel(self):
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignTop)
 
@@ -46,22 +55,89 @@ class App(QMainWindow):
         button = QPushButton("Load")
         layout.addWidget(button)
 
-        container = QGroupBox("Selection Panel")
+        container = QGroupBox("Selection")
+        container.setLayout(layout)
+        return container
+
+    def create_downloads_panel(self):
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+
+        dropdown = QComboBox()
+        dropdown.addItems(["Option 1", "Option 2", "Option 3"])
+        layout.addWidget(dropdown)
+
+        button = QPushButton("Load")
+        layout.addWidget(button)
+
+        container = QGroupBox("Downloads")
+        container.setLayout(layout)
+        return container
+
+    def create_image_panel(self):
+        # Create layout and add image
+        layout = QVBoxLayout()
+        self.image_label = QLabel(self)
+        self.pixmap = QPixmap("data/test.jpg")
+        self.image_label.setPixmap(self.pixmap)
+        layout.addWidget(self.image_label)
+        container = QGroupBox("Image")
+        container.setLayout(layout)
+        return container
+
+    def create_slider_panel(self):
+        # Add lower layout
+        layout = QVBoxLayout()
+        options_layout = QHBoxLayout()
+        slider_layout = QHBoxLayout()
+
+        options_layout.setAlignment(Qt.AlignCenter)
+        slider_layout.setAlignment(Qt.AlignCenter)
+
+        self.toggle_overlay_checkbox = QCheckBox("Toggle Overlay")
+        self.toggle_change_checkbox = QCheckBox("Toggle Overlay")
+
+        options_layout.addWidget(self.toggle_overlay_checkbox)
+        options_layout.addWidget(self.toggle_change_checkbox)
+
+        self.slider = QSlider(Qt.Orientation.Horizontal)
+        self.slider.setTickPosition(QSlider.TicksBelow)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(10)
+
+        self.start_date_label = QLabel("2016")
+        self.end_date_label = QLabel("2021")
+
+        slider_layout.addWidget(self.start_date_label)
+        slider_layout.addWidget(self.slider)
+        slider_layout.addWidget(self.end_date_label)
+
+        layout.addLayout(options_layout)
+        layout.addLayout(slider_layout)
+        container = QGroupBox("Slider")
         container.setLayout(layout)
         return container
 
     def create_right_panel(self):
         layout = QVBoxLayout()
-        layout.addWidget(QLabel("Legend"))
+        legend = self.create_legend_panel()
+        dist = self.create_distribution_panel()
 
-        checkbox1 = QCheckBox("Show Overlay A")
-        checkbox2 = QCheckBox("Show Overlay B")
-        layout.addWidget(checkbox1)
-        layout.addWidget(checkbox2)
+        layout.addWidget(legend)
+        layout.addWidget(dist)
+        return layout
 
+    def create_legend_panel(self):
+        layout = QVBoxLayout()
+
+        container = QGroupBox("Legend")
+        container.setLayout(layout)
+        return container
+
+    def create_distribution_panel(self):
+        layout = QVBoxLayout()
         self.create_bar_chart(layout)
-
-        container = QGroupBox("Settings & Graph")
+        container = QGroupBox("Distribution ")
         container.setLayout(layout)
         return container
 
