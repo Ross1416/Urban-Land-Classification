75d2c8ceb37d29ac36a623222e2c7e33c604559d
sark02 <samuel.kliskey.2020@uni.strath.ac.uk>
2025-03-29 10:36:31 +0000

    Quick Cloud Check Function (Needs Testing)

--- fcns_preprocess.py
+++ fcns_preprocess.py
@@ -7,3 +7,13 @@ def normalise_band_for_CNN(band, mean, std):
     band = np.clip(band,0,1)
 
     return band
+
+def check_cloud(red, green, blue, width, height):
+    gray = 0.2989 * red + 0.5870 * green + 0.1140 * blue
+
+    hist_values, bin_edges = np.histogram(gray, bins=255, range=(0, 255))
+
+    if hist_values[255] > width * height * 0.01:
+        return 1
+    
+    return 0
