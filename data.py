8072add2e2b2e41a615b81c133a1cbe10db232ed
Chris Abiaad <92934215+chrisA777@users.noreply.github.com>
2025-03-29 20:07:11 +0000

    MS RGB model

--- data.py
+++ data.py
@@ -9,7 +9,9 @@ import cv2
 from scipy.interpolate import RectBivariateSpline
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.use("agg")
+
 
 def normalise_band(band, mean, std):
     band = np.nan_to_num(band, nan=0)
@@ -18,6 +20,7 @@ def normalise_band(band, mean, std):
     # band = ((band-np.mean(band))/(np.std(band)+ 1e-8))*std+mean
     # band = np.clip(band,0,1)
     return band
+
 
 def check_cloud(red, green, blue, width, height):
     red = np.clip(red, 0, 2750)
@@ -37,31 +40,40 @@ def check_cloud(red, green, blue, width, height):
 
     return 0
 
-def classify(model, data, class_labels, normalisation_values, RGB_only=False, stride=64):
+
+def classify(
+    model, data, class_labels, normalisation_values, RGB_only=False, stride=64
+):
     class_map = []
 
     # Classify images for each year
     for k in range(data.sizes["t"]):
         # Create matrix for each pixel that keeps track of classes its been labelled
-        classScores = np.zeros((
-            len(data[{"t": 0}].values[0]) + 1,
-            len(data[{"t": 0}].values[0, 0]) + 1,
-            len(class_labels)
-        ))
+        classScores = np.zeros(
+            (
+                len(data[{"t": 0}].values[0]) + 1,
+                len(data[{"t": 0}].values[0, 0]) + 1,
+                len(class_labels),
+            )
+        )
 
         # Create new image with what each pixel was classified as
-        newIm = np.zeros((
-            len(data[{"t": 0}].values[0]) + 1,
-            len(data[{"t": 0}].values[0][0]) + 1
-        ))
+        newIm = np.zeros(
+            (
+                len(data[{"t": 0}].values[0]) + 1,
+                len(data[{"t": 0}].values[0][0]) + 1,
+            )
+        )
 
         # Classify 64*64 patches at every stride
         for i in range(0, len(data[{"t": 0}].values[0]), stride):
             for j in range(0, len(data[{"t": 0}].values[0, 0]), stride):
-                if i + 64 > len(data[{"t": 0}].values[0]) or j + 64 > len(data[{"t": 0}].values[0, 0]):
+                if i + 64 > len(data[{"t": 0}].values[0]) or j + 64 > len(
+                    data[{"t": 0}].values[0, 0]
+                ):
                     continue
 
-                bands = data[{"t": k}].values[:, i:i + 64, j:j + 64]
+                bands = data[{"t": k}].values[:, i : i + 64, j : j + 64]
 
                 # Classify patch
                 bands_arr = []
@@ -82,20 +94,25 @@ def classify(model, data, class_labels, normalisation_values, RGB_only=False, st
                         # print("interpolating")
                         size = int(list(BAND_RESOLUTION.values())[x])
                         # if size != 64:
-                        step = int(64.0/size)
+                        step = int(64.0 / size)
                         # print(size, step)
-                        img_downsampled = band[::step,::step]
+                        img_downsampled = band[::step, ::step]
                         # print(np.array(img_downsampled).shape)
 
-                        x_original = np.linspace(0, 1, size)  # 32 points in original
+                        x_original = np.linspace(
+                            0, 1, size
+                        )  # 32 points in original
                         y_original = np.linspace(0, 1, size)
                         x_new = np.linspace(0, 1, 64)  # 64 target points
                         y_new = np.linspace(0, 1, 64)
 
-                        spline = RectBivariateSpline(y_original, x_original, img_downsampled)
+                        spline = RectBivariateSpline(
+                            y_original, x_original, img_downsampled
+                        )
                         band = spline(y_new, x_new)
                         bands_arr.append(band)
 
+<<<<<<< HEAD
                 rgb_bands.reverse()
                 R = rgb_bands[2]  # Red
                 G = rgb_bands[1]  # Green
@@ -106,6 +123,10 @@ def classify(model, data, class_labels, normalisation_values, RGB_only=False, st
                     if RGB_only:
                         bands_arr.reverse()
                         cnn_image = np.dstack(bands_arr)
+=======
+                # if RGB_only:
+                #   bands_arr.reverse()
+>>>>>>> 5de677f (MS RGB model)
 
                         cnn_image = np.expand_dims(cnn_image, axis=0)
                         # Predict
@@ -128,7 +149,6 @@ def classify(model, data, class_labels, normalisation_values, RGB_only=False, st
                     for cols in range(j, j + 64):
                         classScores[rows, cols, predicted_class] += 1
 
-
         # Iterate through each pixel and check which class it was assigned to most
         for i in range(0, len(newIm) - 1):
             for j in range(0, len(newIm[0]) - 1):
@@ -149,6 +169,7 @@ def classify(model, data, class_labels, normalisation_values, RGB_only=False, st
 
     return class_map
 
+
 def new_coordinates(lat, lon, distance_km, bearing_deg):
     lat_rad = math.radians(lat)
     lon_rad = math.radians(lon)
@@ -156,35 +177,55 @@ def new_coordinates(lat, lon, distance_km, bearing_deg):
 
     R = 6371.0
 
-    new_lat_rad = math.asin(math.sin(lat_rad) * math.cos(distance_km / R) +
-                            math.cos(lat_rad) * math.sin(distance_km / R) * math.cos(bearing_rad))
+    new_lat_rad = math.asin(
+        math.sin(lat_rad) * math.cos(distance_km / R)
+        + math.cos(lat_rad) * math.sin(distance_km / R) * math.cos(bearing_rad)
+    )
 
-    new_lon_rad = lon_rad + math.atan2(math.sin(bearing_rad) * math.sin(distance_km / R) * math.cos(lat_rad),
-                                       math.cos(distance_km / R) - math.sin(lat_rad) * math.sin(new_lat_rad))
+    new_lon_rad = lon_rad + math.atan2(
+        math.sin(bearing_rad) * math.sin(distance_km / R) * math.cos(lat_rad),
+        math.cos(distance_km / R) - math.sin(lat_rad) * math.sin(new_lat_rad),
+    )
 
     new_lat = math.degrees(new_lat_rad)
     new_lon = math.degrees(new_lon_rad)
 
     return new_lat, new_lon
 
+
 def postcode_to_area(postcode, height, width):
-    url = f'https://api.opencagedata.com/geocode/v1/json?q={postcode}&key=ddc3764c2c2942618d17e11d9f0ed724'
+    url = f"https://api.opencagedata.com/geocode/v1/json?q={postcode}&key=ddc3764c2c2942618d17e11d9f0ed724"
     response = requests.get(url)
     data = response.json()
 
-    if data['status']['code'] == 200:
-        location = data['results'][0]['geometry']
+    if data["status"]["code"] == 200:
+        location = data["results"][0]["geometry"]
     else:
         return None
 
-    north, _ = new_coordinates(location['lat'], location['lng'], height / 2, 0)
-    _, east = new_coordinates(location['lat'], location['lng'], width / 2, 90)
-    south, _ = new_coordinates(location['lat'], location['lng'], height / 2, 180)
-    _, west = new_coordinates(location['lat'], location['lng'], width / 2, 270)
+    north, _ = new_coordinates(location["lat"], location["lng"], height / 2, 0)
+    _, east = new_coordinates(location["lat"], location["lng"], width / 2, 90)
+    south, _ = new_coordinates(
+        location["lat"], location["lng"], height / 2, 180
+    )
+    _, west = new_coordinates(location["lat"], location["lng"], width / 2, 270)
 
     return north, south, east, west
 
-def download_dataset(location, width, height, north, south, east, west, bands, cloud_cover, start_year, end_year):
+
+def download_dataset(
+    location,
+    width,
+    height,
+    north,
+    south,
+    east,
+    west,
+    bands,
+    cloud_cover,
+    start_year,
+    end_year,
+):
     con = openeo.connect("openeo.dataspace.copernicus.eu")
     con.authenticate_oidc()
     datacube = None
@@ -198,7 +239,12 @@ def download_dataset(location, width, height, north, south, east, west, bands, c
             print(north,south,east,west)
             datacube = con.load_collection(
                 "SENTINEL2_L2A",
-                spatial_extent={"west": west, "south": south, "east": east, "north": north},
+                spatial_extent={
+                    "west": west,
+                    "south": south,
+                    "east": east,
+                    "north": north,
+                },
                 temporal_extent=temporal_extent,
                 bands=bands,
                 max_cloud_cover=cloud_cover,
@@ -208,6 +254,7 @@ def download_dataset(location, width, height, north, south, east, west, bands, c
         else:
             print(f"Year already downloaded: {saveLocation}")
     return datacube
+
 
 def combine_dataset(location, height, width, start_year, end_year):
     # Combine Dataset
@@ -224,7 +271,9 @@ def combine_dataset(location, height, width, start_year, end_year):
                 maxVal = 2750
 
                 # Convert to Grayscale
-                weights = xr.DataArray([0.2989, 0.5870, 0.1140], dims=["bands"])
+                weights = xr.DataArray(
+                    [0.2989, 0.5870, 0.1140], dims=["bands"]
+                )
                 grayscale = (data[{"t": i}] * weights).sum(dim="bands")
 
                 # (SAME CODE AS CLOUD FUNCTION JUST INPUT IN DIFFERENT FORMAT AND PLOTTING IMAGES HERE)
@@ -234,7 +283,9 @@ def combine_dataset(location, height, width, start_year, end_year):
 
                 grayscale_np = grayscale_np / maxVal * 255
 
-                hist_values, bin_edges = np.histogram(grayscale_np, bins=256, range=(0, 256))
+                hist_values, bin_edges = np.histogram(
+                    grayscale_np, bins=256, range=(0, 256)
+                )
 
                 # If histogram has too much white (cloud) don't include
                 if hist_values[255] > data.shape[-2] * data.shape[-1] * 0.03:
@@ -247,7 +298,10 @@ def combine_dataset(location, height, width, start_year, end_year):
                 #     continue
 
                 # If any in shadow don't include:
-                if np.sum(hist_values[0:5]) > data.shape[-2] * data.shape[-1] * 0.01:
+                if (
+                    np.sum(hist_values[0:5])
+                    > data.shape[-2] * data.shape[-1] * 0.01
+                ):
                     print("Too dark")
                     continue
 
@@ -260,13 +314,29 @@ def combine_dataset(location, height, width, start_year, end_year):
             print(f"Missing {year}")
 
     combined_data = xr.concat(datasets, dim="t")
-    file_path = f"./data/{location}_{height}x{width}_{start_year}to{end_year}.nc"
+    file_path = (
+        f"./data/{location}_{height}x{width}_{start_year}to{end_year}.nc"
+    )
     if file_path is not None:
         combined_data.to_netcdf(file_path)
         print(f"Combined: {file_path}")
 
+
 # ---------------- Global Variables ---------------- #
-ALL_BANDS = ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09","B11", "B12"]
+ALL_BANDS = [
+    "B01",
+    "B02",
+    "B03",
+    "B04",
+    "B05",
+    "B06",
+    "B07",
+    "B08",
+    "B8A",
+    "B09",
+    "B11",
+    "B12",
+]
 RGB_BANDS = ["B04", "B03", "B02"]
 # BAND_NORMALISATION_VALUES = {
 #     "B01": (0.0763954,0.013842635),
@@ -299,18 +369,18 @@ RGB_BANDS = ["B04", "B03", "B02"]
 # }
 
 BAND_RESOLUTION = {
-    "B01": 16,#60, ## SHOULD BE 10
-    "B02": 64,#10,
-    "B03": 64,#10,
-    "B04": 64,#10,
-    "B05": 32,#20,
-    "B06": 32,#20,
-    "B07": 32,#20,
-    "B08": 64,#10,
-    "B8A": 32,#20,
-    "B09": 16,#60, ## SHOULD BE 10
-    "B11": 32,#20,
-    "B12": 32,#20,
+    "B01": 16,  # 60, ## SHOULD BE 10
+    "B02": 64,  # 10,
+    "B03": 64,  # 10,
+    "B04": 64,  # 10,
+    "B05": 32,  # 20,
+    "B06": 32,  # 20,
+    "B07": 32,  # 20,
+    "B08": 64,  # 10,
+    "B8A": 32,  # 20,
+    "B09": 16,  # 60, ## SHOULD BE 10
+    "B11": 32,  # 20,
+    "B12": 32,  # 20,
 }
 
 # RGB_BAND_NORMALISATION_VALUES = {
@@ -318,7 +388,7 @@ BAND_RESOLUTION = {
 #     "B03": (0.3804, 0.1375),
 #     "B04": (0.3398, 0.2037),
 # }
-BAND_NORMALISATION_VALUES ={
+BAND_NORMALISATION_VALUES = {
     "B01": (0.0763954, 0.013842635),
     "B02": (0.4025, 0.1161),
     "B03": (0.3804, 0.1375),
@@ -340,8 +410,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     from tensorflow import keras
 
-    matplotlib.use('qtagg')
-
+    matplotlib.use("qtagg")
 
     MODEL_PATH = "./Classification/eurosat_model.keras"
     FILE_PATH = "./data/Arles_3x3_2016to2023.nc"
@@ -358,8 +427,8 @@ if __name__ == "__main__":
 
     fig, axes = plt.subplots(1, 12, figsize=(12, 9))
     for i, ax in enumerate(axes.flat):
-        ax.imshow(arr[i], cmap='gray')  # Use cmap='gray' for grayscale images
-        ax.axis('off')  # Remove axes for a cleaner look
+        ax.imshow(arr[i], cmap="gray")  # Use cmap='gray' for grayscale images
+        ax.axis("off")  # Remove axes for a cleaner look
 
     plt.tight_layout()
     plt.show()
